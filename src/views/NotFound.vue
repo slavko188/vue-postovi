9efe27416f0ff97e6018b6f72ac02e30c14070eb
Slavko Roganovic <slavkoroganovic234@gmail.com>
2022-12-05 21:40:09 +0100

    add now template

---
+++ src/views/NotFound.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>404</h1>
+    <h3>Page not found</h3>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+div {
+  color: black;
+  text-align: center;
+}
+</style>
